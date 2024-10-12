400cfc034aacac528fc60be985debecd91b74f0f
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2024-10-12 18:24:50 -0500

    Almost functional!

--- crashlink/__init__.py
+++ crashlink/__init__.py
@@ -3,4 +3,6 @@ Just another HashLink decompiler.
 """
 
 from .core import *
+from .errors import *
 from .globals import *
+from .opcodes import *
--- crashlink/errors.py
+++ crashlink/errors.py
@@ -1,15 +1,27 @@
+"""
+crashlink-specific errors.
+"""
+
 
 class CrashlinkError(Exception):
     """
     Base exception class for all errors raised by crashlink.
     """
 
+
 class MalformedBytecode(CrashlinkError):
     """
     Raised when malformed bytecode is deserialised.
     """
 
+
 class NoMagic(CrashlinkError):
     """
     Raised when no magic b"HLB" can be found in a file.
+    """
+
+
+class InvalidOpCode(CrashlinkError):
+    """
+    Raised when an invalid opcode is encountered.
     """
