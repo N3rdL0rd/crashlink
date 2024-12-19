d1f4da331bd4063a0a47dd265b6095bd5eac33c6
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2024-12-19 09:48:05 -0600

    Add tests for SimpleIf and DebugInfo, enhance If class with additional methods, and introduce IR command in CLI

--- tests/haxe/If.hx
+++ tests/haxe/If.hx
@@ -1,12 +1,43 @@
 class If {
+    public static function other() {
+        trace('hi mom');
+    }
+
+    public static function other2() {
+        trace('hi dad');
+    }
+
+    public static function spacer() {
+        trace('spacer');
+    }
+
     static function main() {
         var a = 500;
         if (a > 400) {
             
         }
+        spacer();
         var b = 10;
         if (a > b) {
             
+        }
+        spacer();
+        if (a > 400) {
+            
+        } else {
+            
+        }
+        spacer();
+        if (a > b) {
+            other();
+        } else {
+            other2();
+        }
+        spacer();
+        if (a > 400 && a > b) {
+            other();
+        } else {
+            other2();
         }
     }
 }
