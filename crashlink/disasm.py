3777265fec1407db75e5fc89014178d9062df756
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2024-12-23 12:16:23 -0600

    chore: Add tags to pyproject

--- crashlink/disasm.py
+++ crashlink/disasm.py
@@ -402,7 +402,7 @@ def from_asm(asm: str) -> List[Opcode]:
         opargs = opcodes[op]
         for name, type in opargs.items():
             new_value = Opcode.TYPE_MAP[type]()
-            new_value.value = literal_eval(args.pop(0))
+            new_value.value = literal_eval(args.pop(0)) # FIXME: unsafe?
             new_opcode.definition[name] = new_value
         ops.append(new_opcode)
     return ops
