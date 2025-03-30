538f7b2006ca2bd5d3efe83faa66aa46a557f432
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2025-03-30 11:17:48 -0500

    feat: Autobuild python ish

--- pyhl/build.py
+++ pyhl/build.py
@@ -1,5 +1,6 @@
 import urllib3
 import os
+import subprocess
 
 CURRENT_PYTHON = "https://www.python.org/ftp/python/3.14.0/Python-3.14.0a6.tar.xz"
 DIR = "Python-3.14.0a6"
@@ -36,19 +37,20 @@ def main() -> None:
     print("Downloading Python...")
     download_file(CURRENT_PYTHON, "python.tar.xz")
     print("Extracting...")
-    os.system("tar -xf python.tar.xz")
+    subprocess.run(["tar", "-xf", "python.tar.xz"], check=True)
     print("Configuring...")
     os.chdir(DIR)
     prefix = gen_prefix()
-    os.system("./configure --enable-optimizations --with-ensurepip=install --prefix=" + prefix)
+    configure_cmd = f'./configure --enable-optimizations --with-ensurepip=install --prefix="{prefix}" --disable-test-modules'
+    subprocess.run(configure_cmd, shell=True, check=True)
     print("Building...")
-    os.system("make -j$(($(nproc) + 1))")
+    subprocess.run("make -j$(($(nproc) + 1))", shell=True, check=True)
     print("Installing...")
-    os.system("make install")
+    subprocess.run("make install", shell=True, check=True)
     print("Cleaning up...")
     os.chdir(INITIAL_DIR)
-    os.system("rm -rf " + DIR)
-    os.system("rm python.tar.xz")
+    subprocess.run(["rm", "-rf", DIR], check=True)
+    subprocess.run(["rm", "python.tar.xz"], check=True)
     print("Python built!")
 
 if __name__ == "__main__":
