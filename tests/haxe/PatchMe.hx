aa7682e103234fb616b0adaacbdb002968168437
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2025-04-13 21:38:12 -0500

    wip: _pyhl bindings for hlrun

--- tests/haxe/PatchMe.hx
+++ tests/haxe/PatchMe.hx
@@ -2,12 +2,12 @@ class PatchMe {
     static function main() {
         var val = 1.0;
         var val2 = 2.0;
-        thing(val, val2);
+        thing(val, val2, "Unpatched message!");
     }
 
-    static function thing(val: Float, val2: Float) {
+    static function thing(val: Float, val2: Float, msg: String) {
         if (val == 2.0) {
-            trace("Success!");
+            trace(msg);
         } else {
             trace("Patch failed!");
         }
