b3200c23347f457f69c2fea19fddb84d47419d83
N3rdL0rd <168280212+N3rdL0rd@users.noreply.github.com>
2026-06-22 20:46:20 -0500

    chore(tests): Rearrange stuff to look cleaner in crashtest

--- tests/haxe/VirtualClosure.hx
+++ tests/haxe/VirtualClosure.hx
@@ -1,3 +1,11 @@
+class VirtualClosure {
+    public static function main() {
+        var myObject:Base = new Child();
+        var func = myObject.sayHello;
+        func();
+    }
+}
+
 class Base {
     public function new() {}
 
@@ -13,23 +21,5 @@ class Child extends Base {
 
     override public function sayHello() {
         trace("GREETINGS FROM CHILD!");
-    }
-}
-
-class VirtualClosure {
-    public static function main() {
-        // 1. Create an instance of the child class, but store it in a variable
-        //    typed as the base class. This forces the compiler to consider
-        //    virtual dispatch.
-        var myObject:Base = new Child();
-
-        // 2. THIS IS THE LINE THAT GENERATES OVirtualClosure.
-        //    We are not calling the method directly. Instead, we are getting a
-        //    reference to it, creating a closure. Because the compiler knows
-        //    'sayHello' can be overridden, it must use a virtual lookup.
-        var func = myObject.sayHello;
-
-        // 3. Call the created closure.
-        func(); // This will print "GREETINGS FROM CHILD!"
     }
 }
